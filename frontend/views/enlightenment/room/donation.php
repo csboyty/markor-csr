41eb0995e96bf4bd33b6cd0309c7fbc0c5394378
csboyty <csboyty@gmail.com>
2016-07-04 10:05:52 +0800

    update

--- frontend/views/enlightenment/room/donation.php
+++ frontend/views/enlightenment/room/donation.php
@@ -21,9 +21,33 @@ $this->params=[
 ?>
 <div class="section">
     <h2 class="sectionTitle titleDonationList">捐赠名单</h2>
-    <div class="post">
+    <div class="post"  style="width: 90%">
         <div class="content">
             <?= $result->content; ?>
         </div>
     </div>
 </div>
+<!--<div class="section">
+    <h2 class="sectionTitle titleDonationList">捐赠名单</h2>
+    <div class="post" style="width: 90%">
+        <div class="content">
+            <p style="text-indent: 8%">从2009年“艺术·家”项目启动至2015年底，美克美家先后在西安、沈阳、
+                武汉等21个城市的小学捐赠150间“快乐美术教室”。</p>
+            <p>&nbsp;</p>
+            <p>成都</p>
+            <p>&nbsp;</p>
+            <ul style="margin-left: 8%;">
+                <li style="margin-right: 4%">都江堰市胥家九年制学校</li>
+                <li style="margin-right: 4%">都江堰市天马九年制学校</li>
+                <li style="margin-right: 4%">都江堰市青城山学校</li>
+                <li style="margin-right: 4%">都江堰市中兴九年制学校</li>
+                <li style="margin-right: 4%">都江堰市蒲阳小学金凤校区</li>
+                <li style="margin-right: 4%">都江堰市石羊小学</li>
+                <li style="margin-right: 4%">都江堰市安顺小学</li>
+                <li style="margin-right: 4%">都江堰市聚源小学</li>
+                <li style="margin-right: 4%">都江堰市蒲阳小学</li>
+                <li style="margin-right: 4%">都江堰市崇义小学</li>
+            </ul>
+        </div>
+    </div>
+</div>-->
