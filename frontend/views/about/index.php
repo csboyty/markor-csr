6d0dadd095e14323e2b2ed8831cbe4ab397b1b80
xavieryuan <xavieryuan@xavierdeMacBook-Pro.local>
2016-07-07 06:50:21 +0800

    ui

--- frontend/views/about/index.php
+++ frontend/views/about/index.php
@@ -20,7 +20,7 @@ $this->params=[
         <p class="excerpt">
             美克美家“艺术·家”企业社会责任重点关注视觉艺术领域，包括艺术的认知、发展和保护
             通过对艺术教育欠发达地区的教育、青年艺术家的培养、非物质文化遗产的保护、爱心志愿者团队的支持等模式
-            助推公益性艺术事业的发展
+            助推公益性艺术事业的发展。
         </p>
     </div>
 </div>
@@ -35,58 +35,62 @@ $this->params=[
 
 <div class="section sectionConcern">
     <h2 class="sectionTitle titleConcern">关注</h2>
-    <table class="contentTable">
-        <tr>
-            <td>
-                愿景<br><br>通过艺术的力量<br>
+    <ul class="contentList">
+        <li>
+            <h3>愿景</h3>
+            <p class="line-text">通过艺术的力量<br>
                 引导人们趋向美好的生活<br>
-                促进社会和谐
-            </td>
-            <td>
-                使命<br><br>为身处艺术启蒙、艺术教育<br>
+                促进社会和谐</p>
+        </li>
+        <li>
+            <h3>使命</h3>
+            <p class="line-text">为身处艺术启蒙、艺术教育<br>
                 以及艺术传承阶段的人群<br>
-                提供全面发展的机会
-            </td>
-            <td>
-                核心价值观<br><br>长期的、系统的、以普及教育<br>
+                提供全面发展的机会</p>
+        </li>
+        <li>
+            <h3>核心价值观</h3>
+            <p class="line-text">长期的、系统的、以普及教育<br>
                 和提升社会整体艺术修养为目的的<br>
-                企业社会责任项目
-            </td>
-        </tr>
-    </table>
+                企业社会责任项目</p>
+        </li>
+    </ul>
 </div>
 
 <div class="section">
     <h2 class="sectionTitle titleProgram">项目</h2>
-    <table class="contentTable contentTable1">
-        <tr>
-            <td>
-                <p class="icon"><span class="icon-art-education"></span></p>
-                <br><br>
-                艺术启蒙<br><br>在艺术教育欠发达区域，推进艺术教育工
+    <ul class="contentList">
+        <li>
+            <p class="icon"><span class="icon-art-education"></span></p>
+            <h3>艺术启蒙</h3>
+            <p class="line-text">在艺术教育欠发达区域，推进艺术教育工
                 作，以捐赠艺术类书籍、音像资料等物品
                 及开发艺术教育类课件、培训艺术教师等
-                形式提升青少年的艺术欣赏力。
-            </td>
-            <td>
-                <p class="icon"><span class="icon-modern-art"></span></p>
-                <br><br>
-                艺术教育<br><br>美克美家高等艺术院校设立“艺术·家”奖学
+                形式提升青少年的艺术欣赏力。</p>
+        </li>
+        <li>
+            <p class="icon"><span class="icon-modern-art"></span></p>
+            <h3>艺术教育</h3>
+            <p class="line-text">
+                美克美家高等艺术院校设立“艺术·家”奖学
                 金，奖励和资助对设计有兴趣和天赋的学
                 生。同期，对合作院校大学生开展实习生
                 项目，为其提供社会实践平台。
-            </td>
-            <td>
-                <p class="icon">
+            </p>
+        </li>
+        <li>
+            <p class="icon">
                     <span class="icon-tradition-art"></span>
                 </p>
-                <br><br>
-                艺术传承<br><br>
+            <h3>
+                艺术传承
+            </h3>
+            <p class="line-text">
                 美克美家联手湖南大学，与政府及艺术保
                 护与发展相关机构合作，致力于传统文化
                 遗产保护工作，助推传统艺术的发展与传
                 承。
-            </td>
-        </tr>
-    </table>
+            </p>
+        </li>
+    </ul>
 </div>
